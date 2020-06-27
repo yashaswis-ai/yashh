f334c22063f2e9e8bc80de256b129a51b2f315fb
Yashaswi <Yashaswi@DESKTOP-013N5K5>
2020-06-27 17:23:00 +0530

    committ2

--- DataDrivenFramework/src/com/abc/magento/MagentoLogin.java
+++ DataDrivenFramework/src/com/abc/magento/MagentoLogin.java
@@ -9,12 +9,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MagentoLogin {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		ChromeDriver driver = new ChromeDriver();
+		FirefoxDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
